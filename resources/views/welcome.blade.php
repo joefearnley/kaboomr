aa41d5d0d2467ceba718859786df641df9d1767b
Joe Fearnley <joe.fearnley@gmail.com>
2020-10-21 09:50:12 -0400

    Adding login validation tests

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <div class="container">
         <div class="row justify-content-center align-self-center">
             <h1 class="display-6">Kaboomr - your bookmarks are here.</h1>
+        </div>
+        <div class="row justify-content-center align-self-center">
             <div class="d-flex align-items-center justify-content-center mt-4">
                 <div>
                     <a href="{{ route('login') }}" class="mr-5">
--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -8,16 +8,51 @@ use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+
     public function test_login_page_displays_login_form()
     {
         $response = $this->get(route('login'));
 
         $response->assertStatus(200);
         $response->assertViewIs('auth.login');
+    }
+
+    public function test_login_form_requires_email_and_password()
+    {
+        $formData = [
+            'email' => '',
+            'password' => '',
+        ];
+
+        $response = $this->post('/login', $formData);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
+
+    public function test_login_form_requires_email()
+    {
+        $formData = [
+            'email' => 'john.doe123@gmail.com',
+            'password' => '',
+        ];
+
+        $response = $this->post('/login', $formData);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('password');
+    }
+
+    public function test_login_form_requires_password()
+    {
+        $formData = [
+            'email' => '',
+            'password' => 'secret123',
+        ];
+
+        $response = $this->post('/login', $formData);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
     }
 }
