b8fd69490f542274e6b20d6f88a0f4741dbbe76a
Joe Fearnley <joe.fearnley@gmail.com>
2020-10-21 10:02:28 -0400

    Adding bookmarks controller, model, and migration

---
+++ database/migrations/2020_10_21_135853_create_bookmarks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookmarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->char('name', 150);
+            $table->char('url', 150);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+}
---
+++ resources/views/bookmarks.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center mt-4 align-self-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h2>Delicious</h2>
+                            <p>http://delicious.com</p>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="/bookmarks/edit/1" class="btn btn-secondary mr-3">Edit</a>
+                            <a href="/bookmarks/delete/1" class="btn btn-danger">Delete</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center mt-4 align-self-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h2>Delicious</h2>
+                            <p>http://delicious.com</p>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="/bookmarks/edit/1" class="btn btn-secondary mr-3">Edit</a>
+                            <a href="/bookmarks/delete/1" class="btn btn-danger">Delete</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center mt-4 align-self-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h2>Delicious</h2>
+                            <p>http://delicious.com</p>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="/bookmarks/edit/1" class="btn btn-secondary mr-3">Edit</a>
+                            <a href="/bookmarks/delete/1" class="btn btn-danger">Delete</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
