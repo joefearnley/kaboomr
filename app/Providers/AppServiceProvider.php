d54b710dd1494ecbea80340c4f4374e7632a7708
Joe Fearnley <joe.fearnley@gmail.com>
2020-10-29 20:59:28 -0400

    Adding force https for non-local environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,4 +1,4 @@
-    <?php
+<?php
 
 namespace App\Providers;
 
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
+        if (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
         }
     }
 }
