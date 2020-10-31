169655b7b673329aacac63f67560d569b17b6f87
Joe Fearnley <joe.fearnley@gmail.com>
2020-10-31 09:24:53 -0400

    Roughing in pagination

--- README.md
+++ README.md
@@ -4,16 +4,10 @@ Guess who's back in the roomr.
 
 ### TODO
 
-Bookmarks
- - [x] Edit Bookmark
- - [x] Delete Bookmark
-
-Account
-- [ ] Update Name
-- [ ] Update Email
-- [ ] Change password
-
 Search
 - [ ] search by string
 - [ ] search by tag
 - [ ] search by multiple tags
+
+
+[ ] Pagination
--- resources/views/bookmarks/index.blade.php
+++ resources/views/bookmarks/index.blade.php
@@ -72,6 +72,25 @@
             </div>
         </div>
         @endforeach
+        <div class="row mt-4 justify-content-center">
+            <div class="col-md-4 offset-md-1 my-2">
+                <nav aria-label="pages">
+                    <ul class="pagination">
+                        <li class="page-item disabled">
+                            <span class="page-link">Previous</span>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item active">
+                            <span class="page-link"> 2 <span class="sr-only">(current)</span></span>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     @endif
 </div>
 @endsection
