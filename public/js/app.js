03b39cbf760662476274f4ac87c4b138a41e2369
Joe Fearnley <joe.fearnley@gmail.com>
2020-11-09 21:39:38 -0500

    Removing more comments

--- public/js/app.js
+++ public/js/app.js
@@ -37343,11 +37343,6 @@ try {
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
 
 /***/ }),
 
