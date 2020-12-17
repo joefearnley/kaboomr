9a1e06a921a4f1192bc033944feaf839149ebab3
Joe Fearnley <joe.fearnley@gmail.com>
2020-12-17 12:53:17 -0500

    Adding tests for login attempt for inactive user

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -34,8 +34,10 @@ class AuthTest extends TestCase
 
     public function test_login_form_requires_email()
     {
+        $user = User::factory()->create();
+
         $formData = [
-            'email' => 'john.doe123@gmail.com',
+            'email' => $user->email,
             'password' => '',
         ];
 
@@ -47,9 +49,11 @@ class AuthTest extends TestCase
 
     public function test_login_form_requires_password()
     {
+        $user = User::factory()->create();
+
         $formData = [
             'email' => '',
-            'password' => 'secret123',
+            'password' => 'password',
         ];
 
         $response = $this->post(route('login'), $formData);
@@ -58,6 +62,22 @@ class AuthTest extends TestCase
         $response->assertSessionHasErrors('email');
     }
 
+    public function test_login_form_requires_user_to_be_active()
+    {
+        $user = User::factory()->create([
+            'is_active' => false
+        ]);
+
+        $formData = [
+            'email' => $user->email,
+            'password' => 'password',
+        ];
+
+        $response = $this->post(route('login'), $formData);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
 
     public function test_login_form_redirects_to_bookmarks_page()
     {
@@ -71,7 +91,7 @@ class AuthTest extends TestCase
         $response = $this->post(route('login'), $formData);
 
         $response->assertStatus(302);
-        $response->assertRedirect('/bookmarks');
+        $response->assertRedirect(route('bookmarks.index'));
 
         $this->assertAuthenticatedAs($user);
     }
